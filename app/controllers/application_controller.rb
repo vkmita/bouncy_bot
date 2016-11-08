7d31d012052b440e774d5f2bdf08eecad0e363ef
vkmita <me@vkmita.com>
2016-11-07 20:58:59 -0800

    Add verification endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,18 @@ class ApplicationController < ActionController::API
   end
 
   def verify
+    mode = params[:hub][:mode]
+    verify_token = params[:hub][:verify_token]
+    challenge = params[:hub][:challenge]
+
+    puts mode
+    puts verify_token
+    puts challenge
+
+    if mode == 'subscribe' && verify_token == '417327'
+      render json: { challenge: challenge }
+    else
+      head 401
+    end
   end
 end
